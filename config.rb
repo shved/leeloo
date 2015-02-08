13904974f360f61085c385bf55169816c5446cf5
Vitaly Shvedchenko <vitaly.shvedchenko@gmail.com>
2015-02-08 22:03:55 +0300

    old font files deleted

--- config.rb
+++ config.rb
@@ -52,8 +52,6 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
-set :fonts_dir, 'stylecheets/fonts/'
-
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
